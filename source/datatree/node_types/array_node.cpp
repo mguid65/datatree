684c1d73a2f2165193935ba013598456984707aa
Matthew Guidry <mguid2088@gmail.com>
2024-02-12 19:30:45 -0600

    Make header only

--- source/datatree/node_types/array_node.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @brief Definitions for array node type
- * @author Matthew Guidry (github: mguid65)
- * @date 2024-02-05
- */
-
-#include "datatree/node_types/array_node_type.hpp"
-
-namespace mguid {}  // namespace mguid
--- source/datatree/node_types/value_node.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @brief Definitions for value node type
- * @author Matthew Guidry (github: mguid65)
- * @date 2024-02-05
- */
-
-#include "datatree/node_types/value_node_type.hpp"
-
-namespace mguid {}  // namespace mguid
